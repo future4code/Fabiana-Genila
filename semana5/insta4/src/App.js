78f4df42f6d075e8cee7e772eb3fcb4d9c63e942
Fabiana Genila Ramos Pereira <fabianagenila@gmail.com>
2021-03-10 15:57:17 -0300

    exercicio de React INSTA4 da semana 5, aula 20

---
+++ semana5/insta4/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Post from './components/Post/Post';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'app-container'}>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'fabi'}
+          fotoUsuario={'https://picsum.photos/50/50?random=1'}
+          fotoPost={'https://picsum.photos/200/150?random=1'}
+        />
+
+        <Post
+          nomeUsuario={'wagner'}
+          fotoUsuario={'https://picsum.photos/50/50?random=2'}
+          fotoPost={'https://picsum.photos/200/150?random=2'}
+        />
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ semana5/insta4/src/components/Post/Post.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import './Post.css'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+
+class Post extends React.Component {
+  state = {
+    curtido: false, //sem curtidas, coração branco
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0
+  }
+
+  onClickCurtida = () => {
+    let novoNumeroDeCurtidas
+
+    if(this.state.curtido) { // curtido começa como false, sem curtidas, coração branco
+      novoNumeroDeCurtidas = this.state.numeroCurtidas - 1 //se true (curtido: false), continua 0. Dislike
+    } else {
+      novoNumeroDeCurtidas = this.state.numeroCurtidas + 1 //senão, é false (curtido: true, clica no coração), adiciona um número. Like
+    }
+
+    this.setState({
+      curtido: !this.state.curtido, //Negação: curtido: true. Com curtida, coração preto
+      numeroCurtidas: novoNumeroDeCurtidas
+    })
+
+  }
+
+
+  onClickComentario = () => {
+    this.setState({
+      comentando: !this.state.comentando
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentando: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  render() {
+    let iconeCurtida
+
+    if(this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    }
+
+    return <div className={'post-container'}>
+      <div className={'post-header'}>
+        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p>
+      </div>
+
+      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
+
+      <div className={'post-footer'}>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </div>
+      {componenteComentario}
+    </div>
+  }
+}
+
+export default Post
