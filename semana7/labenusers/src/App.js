6c4c32d4b67533402877f8f53fedb12f2825f250
Fabiana Genila Ramos Pereira <fabianagenila@gmail.com>
2021-03-24 17:52:30 -0300

    exercicio da semana7, aula 27

--- semana7/labenusers/src/App.js
+++ semana7/labenusers/src/App.js
@@ -5,18 +5,39 @@ import ListaUsuario from './components/ListaUsuario'
 
 
 export default class App extends React.Component {
+state = {
+  pagina: "criarUsuario"
+}
+
+  mudarPagina = () => {
+    if (this.state.pagina === "criarUsuario") {
+      this.setState({ pagina: "listaDeUsuarios" });
+    } else if (this.state.pagina === "listaDeUsuarios") {
+      this.setState({ pagina: "criarUsuario" });
+    }
+  };
+
+  renderizarPagina = () => {
+    switch (this.state.pagina) {
+      case "criarUsuario":
+        return <RegistroUsuario />;
+      case "listaDeUsuarios":
+        return <ListaUsuario />;
+      default:
+        return <div></div>;
+    }
+  }
+
 
   render() {
     return(
       <div>
-        <div>
-          <RegistroUsuario />
-          {/* <ListaUsuario /> */}
-        </div>
+        <h1>Labenusers</h1>
+        <button onClick={this.mudarPagina}>Trocar de página</button>
+        {this.renderizarPagina()}
       </div>
     )
-  } 
 }
-
+}
 
 
